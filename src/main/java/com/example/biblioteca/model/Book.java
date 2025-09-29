ea979f111503e8f27f8929ee4bf26a2433464b20
Sofia-Casal <sofia.gutschow@uel.br>
2025-09-28 22:25:37 -0300

    Update Book.java

--- src/main/java/com/example/biblioteca/model/Book.java
+++ src/main/java/com/example/biblioteca/model/Book.java
@@ -4,8 +4,11 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
 import java.time.LocalDate;
 
+@Entity
 public class Book {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotBlank(message = "O título é obrigatório!")
@@ -17,44 +20,64 @@ public class Book {
     private String genero;
     private LocalDate dataInicio;
     private LocalDate dataFim;
-    private boolean terminado;
+
+    private boolean lido;
 
     @Min(value = 0, message = "A nota mínima é 0")
     @Max(value = 5, message = "A nota máxima é 5")
     private int nota = 0;
 
-    @NotBlank(message = "O status de leitura é obrigatório")
-    private String statusLeitura;
-
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getTitulo() {
         return titulo;
     }
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
+    public void setTitulo(String titulo) { this.titulo = titulo; }
+
     public String getAutor() {
         return autor;
     }
     public void setAutor(String autor) {
         this.autor = autor;
     }
+
+    public String getGenero() {
+        return genero;
+    }
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public LocalDate getDataInicio() {
+        return dataInicio;
+    }
+    public void setDataInicio(LocalDate dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public LocalDate getDataFim() {
+        return dataFim;
+    }
+    public void setDataFim(LocalDate dataFim) {
+        this.dataFim = dataFim;
+    }
+
+    public boolean isLido() {
+        return lido;
+    }
+    public void setLido(boolean lido) {
+        this.lido = lido;
+    }
+
     public int getNota() {
         return nota;
     }
     public void setNota(int nota) {
         this.nota = nota;
-    }
-    public boolean isLido() {
-        return terminado;
-    }
-    public void setLido(boolean lido) {
-        this.terminado = lido;
     }
 }
